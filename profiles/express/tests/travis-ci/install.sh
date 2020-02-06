f9f21245c371ef6381ccbf17c0b55bc32006e53d
jeor0980 <jesusotovar@hotmail.com>
2020-02-06 15:43:05 -0700

    Updating to express_mono dev at 41efd5add9d9d8b0fd6356e509742652eb0f517b

--- profiles/express/tests/travis-ci/install.sh
+++ profiles/express/tests/travis-ci/install.sh
@@ -11,8 +11,8 @@ earlyexit
 
 # Build Codebase.
 cd $ROOT_DIR
-drush dl drupal-7.66
-mkdir drupal && mv drupal-7.66/* drupal/
+drush dl drupal-7.68
+mkdir drupal && mv drupal-7.68/* drupal/
 mkdir profiles && mv express_mono drupal/profiles/express
 
 # Harden Core.
